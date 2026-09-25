faa99ba8a93a9e9ecddb37471855204e43748567
MarceloAlvs08 <marcelo.a.a.nunes@gmail.com>
2026-09-24 21:45:43 -0300

    Adicionando multiplicar,py

---
+++ Multiplicar.py
@@ -0,0 +1,8 @@
+def multiplicar(numero1, numero2):
+    resultado = numero1 * numero2
+    return resultado
+
+multiplicacao = multiplicar (9,9)
+
+print(f"O resultado da multiplicação é: {multiplicacao}")
+print(f"Outra soma: {multiplicar (5,10)}")
